998ba8608d5b54161fb11fca4083618842e58934
filippov-da <dfilippov@runity.ru>
2025-11-18 10:46:05 +0500

    Добавил поля к Manifest

--- main/manifest/models.py
+++ main/manifest/models.py
@@ -9,10 +9,37 @@ class Manifest(models.Model):
         related_name="manifest",
         name="app_table_id",
     )
-    icons = models.JSONField(default=list)  # список иконок
+    app_id = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    app_id = models.IntegerField()
-    ...  # TODO добавить остальные поля манифеста
+    pages = models.JSONField()
+    platform_version = models.JSONField()
+
+    dir = models.CharField(
+        max_length=10,
+        choices=[("ltr", "Ltr"), ("rtl", "Rtl"), ("auto", "Auto")],
+        default="auto",
+    )
+    lang = models.CharField(max_length=10, blank=True, default="")
+    short_name = models.CharField(max_length=255, blank=True, default="")
+    description = models.TextField(blank=True, default="")
+    color_scheme = models.CharField(
+        max_length=10,
+        choices=[("dark", "Dark"), ("light", "Light"), ("auto", "Auto")],
+        blank=True,
+        default="",
+    )
+
+    version = models.JSONField(default=dict)
+
+    icons = models.JSONField(default=list)
+    device_type = models.JSONField(default=list)
+    req_permissions = models.JSONField(default=list)
+    widgets = models.JSONField(default=list)
+
+    window = models.JSONField(default=dict)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Manifest for {self.name}"
