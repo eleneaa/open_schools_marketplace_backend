dc754a8a3ee0372fc33f545b7f40b66ad40e0bec
filippov-da <dfilippov@runity.ru>
2025-11-18 21:26:29 +0500

    Add endpoint for appps/ with filters and pagination

--- main/permissions.py
+++ main/permissions.py
@@ -5,7 +5,11 @@ class IsDeveloper(permissions.BasePermission):
     """
     Разрешает доступ только пользователям с is_developer=True
     """
+
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and
-                   hasattr(request.user, 'developer_profile') and
-                   request.user.developer_profile)
+        return bool(
+            request.user
+            and request.user.is_authenticated
+            and hasattr(request.user, "developer_profile")
+            and request.user.developer_profile
+        )
