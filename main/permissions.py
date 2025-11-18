3d5def0e42a6601217eb7ae2a72342e8f53b16ec
filippov-da <dfilippov@runity.ru>
2025-11-18 23:15:15 +0500

    Remove russian comments

--- main/permissions.py
+++ main/permissions.py
@@ -2,9 +2,6 @@ from rest_framework import permissions
 
 
 class IsDeveloper(permissions.BasePermission):
-    """
-    Разрешает доступ только пользователям с is_developer=True
-    """
 
     def has_permission(self, request, view):
         return bool(
