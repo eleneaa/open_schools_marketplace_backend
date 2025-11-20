f87a9e9e5c03a411ec4f2e75c1d7d65f29a28711
filippov-da <dfilippov@runity.ru>
2025-11-20 11:34:51 +0500

    configure apps/{id} endpoint

--- main/apps/serializers.py
+++ main/apps/serializers.py
@@ -1,9 +1,24 @@
+from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
+from app_version.models import AppVersion
+from app_version.serializers import AppVersionSerializer
 from apps.models import App
+from config.serializers import CategorySerializer
 
 
 class AppSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
+    latest_published_version = serializers.SerializerMethodField()
+
+    @extend_schema_field(AppVersionSerializer)
+    def get_latest_published_version(self, obj: App):
+        latest_version = AppVersion.objects.filter(app=obj).order_by("-date").first()
+        if latest_version:
+            return AppVersionSerializer(latest_version).data
+        return None
+
     class Meta:
         model = App
         fields = "__all__"
