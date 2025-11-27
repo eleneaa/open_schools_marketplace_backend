5504f9f832833b607d4afe9faa33d74baf713a05
nepoal <sarkisyan19752005@gmail.com>
2025-11-27 21:05:27 +0500

    fix: delete default django comments, hide vars in env

--- app_settings/config.py
+++ app_settings/config.py
@@ -1,5 +1,8 @@
 import os
 
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-)@#djte1uunyz)-cjrf1tte^ljapdb=t*ejeo)nhs(4ldifxuz")
+DEBUG = os.environ.get("DEBUG", True)
+
 POSTGRES_USER = os.environ.get("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "postgres")
 POSTGRES_DB = os.environ.get("POSTGRES_DB", "postgres")
@@ -8,3 +11,8 @@ POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "localhost")
 POSTGRES_PORT = int(os.environ.get("POSTGRES_PORT", "5432"))
 
 TIME_ZONE = os.environ.get("TIME_ZONE", "UTC")
+
+JWT_ACCESS_TOKEN_LIFETIME = os.environ.get("JWT_ACCESS_TOKEN_LIFETIME", 30)
+JWT_REFRESH_TOKEN_LIFETIME = os.environ.get("JWT_REFRESH_TOKEN_LIFETIME", 10)
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "secretkey")
+JWT_ALGORITHM = os.environ.get("JWT_ALGORITHM", "HS256")
