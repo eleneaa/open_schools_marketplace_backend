541e40929bd558926ef2e4670b15654d4cd329a6
Азизджон <enietogou@gmail.com>
2025-11-28 20:17:00 +0500

    add auth/login/ endpoint

--- main/users/views.py
+++ main/users/views.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from users.jwt_utils import JWTService
-from users.schemes import register_schema
-from users.serializers import RegisterSerializer
+from users.schemes import register_schema, login_schema
+from users.serializers import RegisterSerializer, LoginSerializer, UserProfileSerializer
 
 
 class RegisterView(APIView):
@@ -30,4 +30,31 @@ class RegisterView(APIView):
                 "user": serializer.data,
             },
             status=status.HTTP_201_CREATED,
+        )
+
+
+class LoginView(APIView):
+    @login_schema
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(
+                {"Error": serializer.errors},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        user = serializer.validated_data['user']
+        jwt_service = JWTService()
+        access_token, refresh_token = jwt_service.create_token(user)
+
+        return Response(
+            {
+                "token": {
+                    "access": access_token,
+                    "refresh": refresh_token,
+                },
+                "user": UserProfileSerializer(user).data,
+            },
+            status=status.HTTP_200_OK
         )
